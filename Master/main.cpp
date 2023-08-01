9015a3707fdad8ae21a7e2a5949b229f7b4e32bc
Denis <denis.piotr.stefanski@gmail.com>
2023-08-01 15:24:21 +0200

    Plik Main

---
+++ Master/main.cpp
@@ -0,0 +1,22 @@
+//  Created by Denis Stefanski on 23/06/2023.
+//
+
+#include <iostream>
+#include "Exercise_1v1.hpp"
+#include "Exercise_1v2.hpp"
+
+using namespace std;
+
+
+
+
+
+int main() {
+    
+    Exercise_1v2 a;
+    a.workIt();
+
+ 
+}
+
+
